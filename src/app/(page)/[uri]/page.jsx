022fb5e3efce8694a150bccaa4ff3eda18365616
Noryu <rabiiababsa10@gmail.com>
2023-12-28 17:53:40 +0100

    Publics page styling...

--- src/app/(page)/[uri]/page.jsx
+++ src/app/(page)/[uri]/page.jsx
@@ -1,3 +1,4 @@
+import { Event } from "@/models/event";
 import { Page } from "@/models/page";
 import { User } from "@/models/user";
 import {
@@ -52,6 +53,7 @@ const UserPage = async ({ params }) => {
   mongoose.connect(process.env.MONGODB_URI);
   const page = await Page.findOne({ uri });
   const user = await User.findOne({ email: page.owner });
+  await Event.create({ uri: uri, type: "view" });
 
   return (
     <div className="  min-h-screen">
@@ -77,14 +79,14 @@ const UserPage = async ({ params }) => {
         <FontAwesomeIcon className={"w-6 h-6"} icon={faLocationDot} />
         <span>{page.location}</span>
       </h3>
-      <div className="max-w-xs mx-auto text-center my-2">
+      <div className="max-w-xs text-gray-500/50 mx-auto text-center my-2">
         <p>{page.bio}</p>
       </div>
       <div className="flex gap-2 justify-center items-center pb-4 mt-4">
         {Object.keys(page.buttons).map((buttonKey) => (
           <Link
             href={buttonLink(buttonKey, page.buttons[buttonKey])}
-            className="rounded-full bg-violet-800 border border-2 hover:text-violet-800 hover:bg-white hover:border hover:border-2 text-white p-3 flex justify-center items-center"
+            className="rounded-full bg-violet-800 border border-2 hover:text-violet-800 hover:border-violet-800 hover:bg-white hover:border hover:border-2 text-white p-3 flex justify-center items-center"
           >
             <FontAwesomeIcon
               className={"w-6 h-6"}
@@ -93,9 +95,12 @@ const UserPage = async ({ params }) => {
           </Link>
         ))}
       </div>
-      <div className="max-w-2xl mx-auto grid md:grid-cols-2 gap-6 p-4 px-4">
+      <div className="max-w-2xl mx-auto grid md:grid-cols-2 gap-6 p-4 px-2">
         {page.links.map((link) => (
-          <Link href={link.url} className="bg-violet-800 rounded p-2 block flex">
+          <Link
+            href={link.url}
+            className="bg-violet-800 hover:bg-gray-100 hover:text-violet-800 hover:border-violet-800 border-2 text-white  rounded p-2 block flex"
+          >
             <div className=" relative -left-4 overflow-hidden w-16">
               <div className="w-16 h-16 aspect-square relative flex items-center justify-center">
                 {link.icon && (
@@ -115,9 +120,7 @@ const UserPage = async ({ params }) => {
             <div className="flex items-center justify-center shrink grow-0 overflow-hidden">
               <div>
                 <h3>{link.title}</h3>
-                <p className=" h-6 overflow-hidden">
-                  {link.subtitle}
-                </p>
+                <p className=" h-6 overflow-hidden">{link.subtitle}</p>
               </div>
             </div>
           </Link>
---
+++ src/models/event.jsx
@@ -0,0 +1,11 @@
+const { Schema, models, model } = require("mongoose");
+
+const EventSchema = new Schema(
+  {
+    type: String,
+    uri: String,
+  },
+  { timestamps: true }
+);
+
+export const Event = models?.Event || model("Event", EventSchema);
