ef7f5ecaa7a7eaa8ba17bae03bb7369b8fed96a6
Noryu <rabiiababsa10@gmail.com>
2024-01-03 16:43:42 +0100

    Added analytics chart

--- src/app/(app)/analytics/page.jsx
+++ src/app/(app)/analytics/page.jsx
@@ -1,5 +1,57 @@
-const AnalyticsPage = () => {
-  return <div>Analytics page</div>;
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Chart from "@/components/Chart";
+import SectionBox from "@/components/layout/SectionBox";
+import { Event } from "@/models/event";
+import { Page } from "@/models/page";
+import mongoose from "mongoose";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+const AnalyticsPage = async () => {
+  mongoose.connect(process.env.MONGODB_URI);
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return redirect("/");
+  }
+  const page = await Page.findOne({ owner: session.user.email });
+
+  const groupedViews = await Event.aggregate(
+    [
+      {
+        $match: {
+          type: "view",
+          uri: "Noryu",
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $dateToString: {
+              date: "$createdAt",
+              format: "%Y-%m-%d",
+            },
+          },
+          count: {
+            $count: {},
+          },
+        },
+      },
+    ],
+    { $order: "-_id" }
+  );
+
+  return (
+    <div>
+      <SectionBox>
+        <Chart
+          data={groupedViews.map((object) => ({
+            date: object._id,
+            views: object.count,
+          }))}
+        />
+      </SectionBox>
+    </div>
+  );
 };
 
 export default AnalyticsPage;
--- src/app/api/click/route.jsx
+++ src/app/api/click/route.jsx
@@ -2,7 +2,9 @@ import { Event } from "@/models/event";
 import mongoose from "mongoose";
 
 export async function POST() {
-  mongoose.connect(process.env.MONGODB_URI)
-  await Event.create({ type: "click", uri: "" });
+  mongoose.connect(process.env.MONGODB_URI);
+  const url = new URL(req.url);
+  const clickedLink = atob(url.searchParams.get("url"));
+  await Event.create({ type: "click", uri: clickedLink });
   return Response.json(true);
 }
