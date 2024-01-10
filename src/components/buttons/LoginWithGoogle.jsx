b70494627164792c4d6a8c405f9c649d56bd571b
Noryu <rabiiababsa10@gmail.com>
2024-01-10 16:37:56 +0100

    Login redirect fixed

--- src/components/buttons/LoginWithGoogle.jsx
+++ src/components/buttons/LoginWithGoogle.jsx
@@ -7,7 +7,7 @@ import { signIn } from "next-auth/react";
 const LoginWithGoogle = () => {
   return (
     <button
-      onClick={() => signIn("google")}
+      onClick={() => signIn("google", { callbackUrl: "/" })}
       className="bg-violet-800 text-white text-center w-full py-4 flex gap-3 items-center justify-center"
     >
       <FontAwesomeIcon icon={faGoogle} className="h-6" />
